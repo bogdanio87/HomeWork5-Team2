ba403f759fdde8518c0cd24d306a684ce8853380
Marta <martochka.chorna@gmail.com>
2018-12-08 17:06:54 +0200

    marta

--- HomeWork5/src/main/java/HomeWork5/HomeWork5/App.java
+++ HomeWork5/src/main/java/HomeWork5/HomeWork5/App.java
@@ -1,5 +1,10 @@
 package HomeWork5.HomeWork5;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ArrayList<String> students = new ArrayList<String>();
+        try (BufferedReader task2 = new BufferedReader(new FileReader("src/main/java/HomeWork5/HomeWork5/HWork5/students.txt")))
+        {
+
+            String sCurrentLine;
+
+            while ((sCurrentLine = task2.readLine()) != null) {
+                students.add(sCurrentLine);
+                
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+        System.out.println("ArrayList: " + students);
     }
-}
+    }
+
