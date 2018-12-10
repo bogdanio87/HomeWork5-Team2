5da1b206e75b3c7f552f8f6ad426189a88654f61
MykolaOdynets <44775020+MykolaOdynets@users.noreply.github.com>
2018-12-10 23:44:53 +0200

    Update App.java

--- HomeWork5/src/main/java/HomeWork5/HomeWork5/App.java
+++ HomeWork5/src/main/java/HomeWork5/HomeWork5/App.java
@@ -4,32 +4,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        ArrayList<String> students = new ArrayList<String>();
-        try (BufferedReader task2 = new BufferedReader(new FileReader("src/main/java/HomeWork5/HomeWork5/HWork5/students.txt")))
-        {
-
-            String sCurrentLine;
-
-            while ((sCurrentLine = task2.readLine()) != null) {
-                students.add(sCurrentLine);
-                
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } 
-        System.out.println("ArrayList: " + students);
-    }
-    }
+public class App {
+	public static void main(String[] args) {
+		ArrayList<String> students = new ArrayList<String>();
+		try (BufferedReader task2 = new BufferedReader(
+				new FileReader("src/main/java/HomeWork5/" + "HomeWork5/HWork5/students.txt"))) {
+			String sCurrentLine;
+			while ((sCurrentLine = task2.readLine()) != null) {
+				students.add(sCurrentLine.trim());
+			}
+		} catch (IOException e) {
+			System.out.println("File not exist ! ! !");
+		}
+		Collections.sort(students);
+		System.out.println("ArrayList: " + students);
+	}
+}
 
